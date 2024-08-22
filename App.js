326658a870deb63e58fa3182f3b1edf527a06ce6
Emmanuel Pius <emmycode4495@gmail.com>
2024-08-22 13:02:24 +0100

    Project creation and expo prebuild integration

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View style={{ flex:1, justifyContent:'center', alignItems:'center', margintop:100}}>
+      <Text>App</Text>
+    </View>
+  )
+}
